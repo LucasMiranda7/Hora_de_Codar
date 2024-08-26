cb1bb8d07f344149fb065ee2c51568b3ccd7bc40
Lucas Miranda <143975592+LucasMiranda7@users.noreply.github.com>
2024-08-26 18:57:35 -0300

    Add files via upload

--- [Kotlin] Hora de Codar 2/src/atv9.kt
+++ [Kotlin] Hora de Codar 2/src/atv9.kt
@@ -1,13 +1,26 @@
 fun main(){
     //exercicio 9
 
+    println("\n---> URNA ELETRÔNICA < --- \n")
+
+    println("Você possuí Titulo Eleitoral: (SIM/NÃO)")
+    var titulo_eleitoral: String = readln().toString()
+
+    if(titulo_eleitoral == "s"){
+      println("Você Possui titulo eleitoral\n")
+    }
+    else {
+        println("Você não tem titulo eleitoral. Não pode votar!\n")
+        return
+    }
+
     println("Insira seu Ano de Nascimento: ")
     var ano_nascimento = readln().toInt()
     val anoAtual = 2024
     var calcIdade = anoAtual - ano_nascimento
 
     if(calcIdade >= 16){
-        println("Pode votar")
+        println("Pode votar, pois sua idade é $calcIdade")
     }
     else{
         println("Não pode votar!")
